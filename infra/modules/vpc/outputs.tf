1da66c54660d820209d2a5e954477f4f30b40278
shivajikhot <shivajkv@gmail.com>
2025-01-29 23:30:59 +0530

    Update outputs.tf

--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id[count.index]
+  value = aws_subnet.public_subnet[count.index]
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id[count.index]
+  value = aws_subnet.private_subnet[count.index]
 }
