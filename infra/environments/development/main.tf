849f8247c12493d63ad13ba16382334cf81add1a
shivajikhot <shivajkv@gmail.com>
2025-01-29 17:38:42 +0530

    Update main.tf

--- infra/environments/development/main.tf
+++ infra/environments/development/main.tf
@@ -33,7 +33,7 @@ module "ecr" {
 
 module "alb" {
   source                = "../../modules/alb"
-  security_group_id     = module.iam.alb_security_group_id
+  alb_security_group_id     = module.iam.alb_security_group_id
   public_subnet_id      = module.vpc.public_subnet_id
   vpc_id                = module.vpc.vpc_id
   environment           = "development"
