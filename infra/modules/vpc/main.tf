ab3071efa19669875bddedbaaa8ed23a9aa5cb16
shivajikhot <shivajkv@gmail.com>
2025-01-29 19:42:44 +0530

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_subnet" {
   count                   = length(var.availability_zones)
-  vpc_id                  = var.vpc_id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = element(var.public_subnet_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = true
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   count                   = length(var.availability_zones)
-  vpc_id                  = var.vpc_id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = element(var.private_subnet_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
   tags = {
