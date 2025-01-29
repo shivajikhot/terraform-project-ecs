bacbf647ff2c685f8979ca5ec324bf68ddb5c5a8
shivajikhot <shivajkv@gmail.com>
2025-01-29 23:57:06 +0530

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -28,3 +28,31 @@ resource "aws_subnet" "private_subnet" {
     Name = "${var.environment}-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public_subnet)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
