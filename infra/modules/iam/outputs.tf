61e198ec90e9a307c12e3a9d75450559fe91debe
shivajikhot <shivajkv@gmail.com>
2025-01-30 00:18:55 +0530

    Update outputs.tf

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -5,3 +5,11 @@ output "ecs_security_group_id" {
 output "alb_security_group_id" {
   value = aws_security_group.alb_security_group.id
 }
+
+output "ecs_execution_role_arn" {
+  value = aws_iam_role.ecs_execution_role.arn
+}
+
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
