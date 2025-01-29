3b011106706ec5a22bbaf23500234bbcfa016341
shivajikhot <shivajkv@gmail.com>
2025-01-29 19:45:18 +0530

    Update main.tf

--- infra/environments/development/main.tf
+++ infra/environments/development/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  availability_zone   = var.availability_zone
+  availability_zone   = var.availability_zones
   environment         = var.environment
 }
 #module "ecs" {
