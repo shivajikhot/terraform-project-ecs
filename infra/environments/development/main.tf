8a88d0cf7e031af16e033086a985d89fc6355b5a
shivajikhot <shivajkv@gmail.com>
2025-01-29 17:45:17 +0530

    Update main.tf

--- infra/environments/development/main.tf
+++ infra/environments/development/main.tf
@@ -8,8 +8,7 @@ module "vpc" {
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   availability_zone   = var.availability_zone
-  environment         = "development"
-}
+  environment         = var.environment
 
 module "ecs" {
   source                  = "../../modules/ecs"
@@ -17,13 +16,13 @@ module "ecs" {
   appointment_service_image = var.appointment_service_image
   public_subnet_id        = module.vpc.public_subnet_id
   ecs_security_group_id   = module.iam.ecs_security_group_id
-  environment             = "development"
+  environment             = var.environment
 }
 
 module "iam" {
   source                      = "../../modules/iam"
   execution_role_policy_arn   = var.execution_role_policy_arn
-  environment                 = "development"
+  environment                 = var.environment"
 }
 
 module "ecr" {
@@ -37,5 +36,5 @@ module "alb" {
   alb_security_group_id     = module.iam.alb_security_group_id
   public_subnet_id      = module.vpc.public_subnet_id
   vpc_id                = module.vpc.vpc_id
-  environment           = "development"
+  environment           = var.environment
 }
