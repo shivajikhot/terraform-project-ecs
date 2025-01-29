6f7e98227698bed00ba16b23859edd8adce7cb81
shivajikhot <shivajkv@gmail.com>
2025-01-30 00:38:58 +0530

    Update outputs.tf

--- infra/modules/ecs/outputs.tf
+++ infra/modules/ecs/outputs.tf
@@ -5,3 +5,6 @@ output "ecs_cluster_id" {
 output "ecs_service_name" {
   value = aws_ecs_service.ecs_service.name
 }
+output "ecs_task_definition_arn" {
+  value = aws_ecs_task_definition.task_definition.arn
+}
