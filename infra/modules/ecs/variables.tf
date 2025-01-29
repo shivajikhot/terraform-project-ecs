a3a931000ef34d4369919409046c175184b8400a
shivajikhot <shivajkv@gmail.com>
2025-01-30 00:05:14 +0530

    Update variables.tf

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -1,7 +1,3 @@
-variable "patient_service_image" {
-  description = "Docker image URL for the patient service"
-  type = string
-}
 
 variable "public_subnet_id" {
   description = "The public subnet ID"
@@ -16,8 +12,4 @@ variable "ecs_security_group_id" {
 variable "environment" {
   description = "Environment name (e.g., dev, staging, prod)"
   type = string
-}
-variable "appointment_service_image" {
-  description = "The Docker image URL for the appointment service"
-  type        = string
 }
