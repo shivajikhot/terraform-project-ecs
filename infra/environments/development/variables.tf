8174fa150d13ee895b457d422a4b99e5b7f7c876
shivajikhot <shivajkv@gmail.com>
2025-01-29 23:12:50 +0530

    Update variables.tf

--- infra/environments/development/variables.tf
+++ infra/environments/development/variables.tf
@@ -5,12 +5,12 @@ variable "vpc_cidr" {
 
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
-  type        = string
+  type        = list(string)
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
-  type        = string
+  type        = list(string)
 }
 
 variable "availability_zones" {
