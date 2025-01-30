96e9d249d0893fa8e3735f237797bd5eede288a6
shivajikhot <shivajkv@gmail.com>
2025-01-30 16:31:39 +0530

    Update output.tf

--- infra/modules/alb/output.tf
+++ infra/modules/alb/output.tf
@@ -1,3 +1,11 @@
 output "alb_dns_name" {
   value = aws_lb.application_load_balancer.dns_name  # Fix reference
 }
+
+output "patient_tg_arn" {
+  value = aws_lb_target_group.patient_tg.arn
+}
+
+output "appointment_tg_arn" {
+  value = aws_lb_target_group.appointment_tg.arn
+}
