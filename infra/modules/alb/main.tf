2ed61ce7a3669f486e4ac6415513e3a442d7bbf4
shivajikhot <shivajkv@gmail.com>
2025-01-30 16:30:25 +0530

    Update main.tf

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -11,26 +11,65 @@ resource "aws_lb" "application_load_balancer" {
   }
 }
 
-resource "aws_lb_target_group" "target_group" {
-  name     = "${var.environment}-target-group"
-  port     = 80
+resource "aws_lb_target_group" "patient_tg" {
+  name     = "patient-tg"
+  port     = 3000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-  target_type = "ip"  # Required for Fargate
-  protocol_version = "HTTP1"
+  target_type = "ip"
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.application_load_balancer.arn  # FIXED REFERENCE
-  port              = "80"
+resource "aws_lb_target_group" "appointment_tg" {
+  name     = "appointment-tg"
+  port     = 3001
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+}
+
+# Single ALB Listener on port 80
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
     fixed_response {
-      status_code  = 200
+      status_code  = 404
       content_type = "text/plain"
-      message_body = "OK"
+      message_body = "Not Found"
     }
+  }
+}
+resource "aws_lb_listener_rule" "patient_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["/patient*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.patient_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "appointment_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 200
+
+  condition {
+    path_pattern {
+      values = ["/appointment*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.appointment_tg.arn
   }
 }
