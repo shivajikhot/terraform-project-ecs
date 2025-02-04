74f7e4cb2da4391bb2709a02b46256cd1acab9d9
shivajikhot <shivajkv@gmail.com>
2025-02-04 16:25:22 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "alb_security_group" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
-
+  }
   egress {
     from_port   = 0
     to_port     = 0
