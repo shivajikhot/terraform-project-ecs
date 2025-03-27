9c642fe437f47fb5d1a53158446e9f6fce8bf093
shivajikhot <shivajkv@gmail.com>
2025-03-27 13:39:52 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,7 +1,4 @@
-resource "aws_ecr_repository" "patient_service_repo" {
-  name = "${var.environment}-patient-service-repo"
+resource "aws_ecr_repository" "openproject_service_repo" {
+  name = "${var.environment}-open_project_repo"
 }
 
-resource "aws_ecr_repository" "appointment_service_repo" {
-  name = "${var.environment}-appointment-service-repo"
-}
