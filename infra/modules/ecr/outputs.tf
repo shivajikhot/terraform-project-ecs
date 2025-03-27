e7ddc90162e6c368cb70a9b8f4ecb496cc3f902d
shivajikhot <shivajkv@gmail.com>
2025-03-27 13:50:31 +0530

    Update outputs.tf

--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -1,7 +1,3 @@
-output "patient_service_repo_url" {
-  value = aws_ecr_repository.patient_service_repo.repository_url
-}
-
-output "appointment_service_repo_url" {
-  value = aws_ecr_repository.appointment_service_repo.repository_url
+output "openproject_service_repo_url" {
+  value = aws_ecr_repository.openproject_service_repo.repository_url
 }
