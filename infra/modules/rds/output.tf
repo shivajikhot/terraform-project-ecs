f65307347962b0c4f77d89dab6dc5e7a501ced40
shivajikhot <shivajkv@gmail.com>
2025-03-31 11:47:49 +0530

    Update output.tf

--- infra/modules/rds/output.tf
+++ infra/modules/rds/output.tf
@@ -2,13 +2,6 @@ output "aurora_cluster_endpoint" {
   value = aws_rds_cluster.openproject.endpoint
 }
 
-output "aurora_writer_endpoint" {
-  value = aws_rds_cluster.openproject.writer_endpoint
-}
-
-output "aurora_reader_endpoint" {
-  value = aws_rds_cluster.openproject.reader_endpoint
-}
 
 output "db_subnet_group_name" {
   value = aws_db_subnet_group.openproject.name
