21f0e8d12f5b8d2e1cdc5f2e04e099dc3715de02
shivajikhot <shivajkv@gmail.com>
2025-03-31 11:32:22 +0530

    Update variables.tf

--- infra/environments/development/variables.tf
+++ infra/environments/development/variables.tf
@@ -90,3 +90,28 @@ variable "instance_class" {
   type        = string
   default     = "db.t3.medium"
 }
+#############################
+variable "cluster_id" {
+  description = "The unique identifier for the ElastiCache cluster."
+  type        = string
+}
+
+variable "node_type" {
+  description = "The compute and memory capacity of the cache nodes (e.g., cache.t3.micro, cache.t3.small, etc.)."
+  type        = string
+  default     = "cache.t3.micro"
+}
+
+variable "num_cache_nodes" {
+  description = "The number of cache nodes in the ElastiCache cluster. Must be greater than 0."
+  type        = number
+  default     = 1
+}
+
+variable "parameter_group_name" {
+  description = "The name of the parameter group for the ElastiCache cluster."
+  type        = string
+  default     = "default.memcached1.6"
+}
+
+
