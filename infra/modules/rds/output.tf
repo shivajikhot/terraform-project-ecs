6f89078d79ad1691b9089699d315177252ca5890
shivajikhot <shivajkv@gmail.com>
2025-03-31 13:27:56 +0530

    Update output.tf

--- infra/modules/rds/output.tf
+++ infra/modules/rds/output.tf
@@ -6,3 +6,13 @@ output "aurora_cluster_endpoint" {
 output "db_subnet_group_name" {
   value = aws_db_subnet_group.openproject.name
 }
+output "cluster_master_username" {
+  description = "Aurora PostgreSQL master username"
+  value       = aws_rds_cluster.openproject.master_username
+}
+
+output "cluster_master_password" {
+  description = "Aurora PostgreSQL master password"
+  value       = aws_rds_cluster.openproject.master_password
+  sensitive   = true
+}
