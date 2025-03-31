efca52fe54ed92086c4b1dcc5f404fe06cbc5422
shivajikhot <shivajkv@gmail.com>
2025-03-31 12:24:09 +0530

    Update main.tf

--- infra/modules/elasticache/main.tf
+++ infra/modules/elasticache/main.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "memcached" {
   node_type           = var.node_type
   num_cache_nodes     = var.num_cache_nodes
   parameter_group_name = var.parameter_group_name
-  subnet_group_name   = aws_elasticache_subnet_group.openproject
+  subnet_group_name   = aws_elasticache_subnet_group.openproject.name
   security_group_ids  = [var.security_group_id]
 }
 resource "aws_elasticache_subnet_group" "openproject" {
